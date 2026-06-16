e653923d273a32853d735ac2d031b57f114a2244
Dennis <dennisokaforab@yahoo.com>
2026-06-16 02:16:52 +0100

    Initial Commit

---
+++ CreatorInc/CreatorIncApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreatorIncApp.swift
+//  CreatorInc
+//
+//  Created by Dennis Okafor on 16/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct CreatorIncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
